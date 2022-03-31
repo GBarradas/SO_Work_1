f8b89245f527567550606b7d3c1ca14e3f4ca401
Gbarradas <gbarradas1@hotmail.com>
2022-03-31 16:36:38 +0100

    2022-03-31 16:36:38.667491

--- Queue.c
+++ Queue.c
@@ -4,8 +4,71 @@
 #include"process.c"
 #include"fatal.h"
 
-
 typedef bool Boolean;
+typedef struct node *Node;
+typedef struct queue *Queue;
 
+struct node{
+    Process element;
+    Node *next;
+};
 
+struct queue{
+    int size;
+    Node *Rear;
+    Node *Head;
+};
+
+Queue inicializeQueue(){
+    Queue Q = malloc( sizeof(Queue) ) ;
+    Q->size = 0;
+    Q->Head = NULL;
+    Q->Rear = NULL;
+    return Q;
+}
+
+Boolean isEmpty(Queue Q){
+    return Q->size == 0;
+}
+
+void Enqueue(Process P, Queue Q){
+    Node N = malloc( sizeof(Node));
+    Node aux;
+    N->element = P;
+    N->next = NULL;
+    if(Q->size == 0){
+        Q->Head = N;
+        Q->Rear = N ;
+        ++Q->size;
+    }
+    else{
+        aux = Q->Rear;
+        aux->next = N;
+        Q->Rear = N;
+        ++Q->size;
+    }
+}
+
+Process peek(Queue Q){
+    Node aux = Q->Head;
+    return aux->element;
+}
+
+void Dequeue(Queue Q){
+    if(!isEmpty(Q)){
+        if(Q->size == 1){
+            Node aux = Q->Head;
+            Q->Head = NULL;
+            Q->Rear = NULL;
+            --Q->size;
+            free(aux);
+        }
+        else{
+            Node aux = Q->Head;
+            Q-> Head = aux->next;
+            --Q->size;
+            free(aux);
+        }
+    }
+}
 
--- process.c
+++ process.c
@@ -3,6 +3,16 @@
 
 
 typedef struct process * Process;
+typedef struct so *SO;
+enum States
+{
+    NEW,
+    READY,
+    RUN,
+    BLOCKED,
+    EXIT,
+    
+};
 
 struct process{
     int a;
